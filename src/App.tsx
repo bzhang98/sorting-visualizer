0e211e39b30b2837acad711c70dc8e17e897487f
bzhang98 <105823390+bzhang98@users.noreply.github.com>
2024-10-17 06:26:03 -0500

    create new heap-sort path

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import BubbleSort from "./components/BubbleSort";
 import SelectionSort from "./components/SelectionSort";
 import InsertionSort from "./components/InsertionSort";
+import HeapSort from "./components/HeapSort";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Link } from "react-router-dom";
 
@@ -18,20 +19,27 @@ export default function App() {
           <Link className="hover:underline" to="/insertion-sort">
             Insertion Sort
           </Link>
+          <Link className="hover:underline" to="/heap-sort">
+            Heap Sort
+          </Link>
         </nav>
       </header>
       <Routes>
         <Route
           path="/bubble-sort"
-          element={<BubbleSort maxValue={100} numBars={50} />}
+          element={<BubbleSort maxValue={100} numBars={100} speed={10} />}
         />
         <Route
           path="/selection-sort"
-          element={<SelectionSort maxValue={100} numBars={100} />}
+          element={<SelectionSort maxValue={100} numBars={100} speed={10} />}
         />
         <Route
           path="/insertion-sort"
-          element={<InsertionSort maxValue={100} numBars={100} />}
+          element={<InsertionSort maxValue={100} numBars={100} speed={10} />}
+        />
+        <Route
+          path="/heap-sort"
+          element={<HeapSort maxValue={100} numBars={100} speed={10} />}
         />
       </Routes>
     </Router>
