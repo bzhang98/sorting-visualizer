7b1c0be33c796903b3f89aa823c45bb644ce6906
bzhang98 <105823390+bzhang98@users.noreply.github.com>
2024-10-21 17:56:44 -0500

    change state to be provided by context

--- src/App.tsx
+++ src/App.tsx
@@ -3,34 +3,15 @@ import SelectionSort from "./components/SelectionSort";
 import InsertionSort from "./components/InsertionSort";
 import HeapSort from "./components/HeapSort";
 import MergeSort from "./components/MergeSort";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { useCallback, useState } from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import QuickSort from "./components/QuickSort";
 import useWindowWidth from "./hooks/use-window-width";
+import { OptionsProvider } from "./context/options-context";
 
 export default function App() {
-  const [speed, setSpeed] = useState(1);
-  const [numBars, setNumBars] = useState(30);
-  const [tempSpeed, setTempSpeed] = useState(speed);
-  const [tempNumBars, setTempNumBars] = useState(numBars);
-
   const windowWidth = useWindowWidth();
 
-  const handleFinishChange = useCallback(
-    (setter: (arg0: number) => void, value: any) => {
-      setter(Number(value));
-    },
-    []
-  );
-
-  const handleChange = useCallback(
-    (setTemp: (arg0: number) => void, value: any) => {
-      setTemp(Number(value));
-    },
-    []
-  );
-
-  if (windowWidth < 768) {
+  if (windowWidth < 750) {
     return (
       <div className="p-8">
         <h1 className="text-2xl font-semibold">Please use a larger screen</h1>
@@ -39,72 +20,25 @@ export default function App() {
   }
 
   return (
-    <Router>
-      <div className="px-8 flex flex-col gap-4 w-1/3">
-        <h2 className="text-2xl font-semibold">Settings</h2>
-        <div className="flex flex-col gap-2">
-          <label htmlFor="numBars">Number of Elements: {tempNumBars}</label>
-          <input
-            type="range"
-            name="numBars"
-            id="numBars"
-            min={5}
-            max={100}
-            step={1}
-            value={tempNumBars}
-            onChange={(e) => handleChange(setTempNumBars, e.target.value)}
-            onMouseUp={() => handleFinishChange(setNumBars, tempNumBars)}
-            onTouchEnd={() => handleFinishChange(setNumBars, tempNumBars)}
-          />
+    <OptionsProvider>
+      <Router>
+        <div className="max-w-full links flex gap-8 p-8">
+          <Link to="/bubble-sort">Bubble Sort</Link>
+          <Link to="/selection-sort">Selection Sort</Link>
+          <Link to="/insertion-sort">Insertion Sort</Link>
+          <Link to="/heap-sort">Heap Sort</Link>
+          <Link to="/merge-sort">Merge Sort</Link>
+          <Link to="/quick-sort">Quick Sort</Link>
         </div>
-        <div className="flex flex-col gap-2">
-          <label htmlFor="speed">Speed: {tempSpeed}x</label>
-          <input
-            type="range"
-            name="speed"
-            id="speed"
-            min={0.25}
-            max={10}
-            step={0.05}
-            value={tempSpeed}
-            onChange={(e) => handleChange(setTempSpeed, e.target.value)}
-            onMouseUp={() => handleFinishChange(setSpeed, tempSpeed)}
-            onTouchEnd={() => handleFinishChange(setSpeed, tempSpeed)}
-          />
-        </div>
-      </div>
-      <Routes>
-        <Route
-          path="/bubble-sort"
-          element={
-            <BubbleSort maxValue={100} numBars={numBars} speed={speed} />
-          }
-        />
-        <Route
-          path="/selection-sort"
-          element={
-            <SelectionSort maxValue={100} numBars={numBars} speed={speed} />
-          }
-        />
-        <Route
-          path="/insertion-sort"
-          element={
-            <InsertionSort maxValue={100} numBars={numBars} speed={speed} />
-          }
-        />
-        <Route
-          path="/heap-sort"
-          element={<HeapSort maxValue={100} numBars={numBars} speed={speed} />}
-        />
-        <Route
-          path="/merge-sort"
-          element={<MergeSort maxValue={100} numBars={numBars} speed={speed} />}
-        />
-        <Route
-          path="/quick-sort"
-          element={<QuickSort maxValue={100} numBars={numBars} speed={speed} />}
-        />
-      </Routes>
-    </Router>
+        <Routes>
+          <Route path="/bubble-sort" element={<BubbleSort />} />
+          <Route path="/selection-sort" element={<SelectionSort />} />
+          <Route path="/insertion-sort" element={<InsertionSort />} />
+          <Route path="/heap-sort" element={<HeapSort />} />
+          <Route path="/merge-sort" element={<MergeSort />} />
+          <Route path="/quick-sort" element={<QuickSort />} />
+        </Routes>
+      </Router>
+    </OptionsProvider>
   );
 }
